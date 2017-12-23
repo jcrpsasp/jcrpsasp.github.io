1074c6e2758b12c2c9299aca0f0e3a2cc74af68d
jcrpsasp <34761839+jcrpsasp@users.noreply.github.com>
2017-12-22 21:53:17 -0600

    Add files via upload

---
+++ layout/scripts/cclsearch.js
@@ -0,0 +1,29 @@
+﻿var states = [
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Valid",
+	"Expired",
+	"Expired",
+	"Expired",
+	"Expired",
+	"Revoked",
+	"Revoked"
+]
+
+function newState() {
+	var getState = Math.floor(Math.random() * (states.length));
+	document.getElementById("stateDisplay").innerHTML = states[getState];
+}
+
